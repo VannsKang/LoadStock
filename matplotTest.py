de249d6bc2fad59a546876e590f22f8bcd8b8c8f
Soomyung Kang <vannskang@gmail.com>
2020-07-15 16:08:11 +0900

    matplot

---
+++ matplotTest.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# plt.plot([1, 2, 3, 4])
+# plt.show()
+
+# x = range(0, 100)
+# y = [v*v for v in x]
+# plt.plot(x, y, 'g+')
+# plt.show()
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+plt.show()
